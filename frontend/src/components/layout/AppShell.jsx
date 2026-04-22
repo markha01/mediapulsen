ccae89d20722d41dce04a70f11a167d6e8a242b3
Maryam Khan <markha@kth.se>
2026-04-22 12:29:20 +0200

    Added useEffect that fetches all datasets on app entry

--- frontend/src/components/layout/AppShell.jsx
+++ frontend/src/components/layout/AppShell.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import Sidebar from './Sidebar';
 import TopBar from './TopBar';
 import { Outlet } from 'react-router-dom';
+import client from '../../api/client';
+import useAppStore from '../../store/appStore';
 
 export default function AppShell() {
+  const { setDatasets, setActiveDatasetId, activeDatasetId } = useAppStore();
+
+  useEffect(() => {
+    client.get('/api/datasets').then((res) => {
+      setDatasets(res.data);
+      if (!activeDatasetId && res.data.length > 0) {
+        setActiveDatasetId(res.data[0].id);
+      }
+    }).catch(() => {});
+  }, []);
+
   return (
     <div className="flex h-screen bg-bg overflow-hidden">
       <Sidebar />
